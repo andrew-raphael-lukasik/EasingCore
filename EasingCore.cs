39798256c1da0a8f49aab67dfbb7a580fcc49ba0
Andrew Raphael Lukasik <andrew.r.lukasik@gmail.com>
2020-01-30 16:02:58 +0100

    refactored

--- EasingCore.cs
+++ EasingCore.cs
@@ -1,196 +1,116 @@
-﻿/*
- * EasingCore (https://github.com/setchi/EasingCore)
- * Copyright (c) 2019 setchi
- * Licensed under MIT (https://github.com/setchi/EasingCore/blob/master/LICENSE)
- */
+// EasingCore (https://github.com/setchi/EasingCore)
+// Copyright (c) 2019 setchi
+// Licensed under MIT (https://github.com/setchi/EasingCore/blob/master/LICENSE)
 
 using UnityEngine;
+using Unity.Mathematics;
 
 namespace EasingCore
 {
-    public enum Ease
+    public static class Ease
     {
-        Linear,
-        InBack,
-        InBounce,
-        InCirc,
-        InCubic,
-        InElastic,
-        InExpo,
-        InQuad,
-        InQuart,
-        InQuint,
-        InSine,
-        OutBack,
-        OutBounce,
-        OutCirc,
-        OutCubic,
-        OutElastic,
-        OutExpo,
-        OutQuad,
-        OutQuart,
-        OutQuint,
-        OutSine,
-        InOutBack,
-        InOutBounce,
-        InOutCirc,
-        InOutCubic,
-        InOutElastic,
-        InOutExpo,
-        InOutQuad,
-        InOutQuart,
-        InOutQuint,
-        InOutSine,
-    }
 
-    public delegate float EasingFunction(float t);
+		public static float Linear ( float t ) => t;
 
-    public static class Easing
-    {
-        /// <summary>
-        /// Gets the easing function
-        /// </summary>
-        /// <param name="type">Ease type</param>
-        /// <returns>Easing function</returns>
-        public static EasingFunction Get(Ease type)
-        {
-            switch (type)
-            {
-                case Ease.Linear: return linear;
-                case Ease.InBack: return inBack;
-                case Ease.InBounce: return inBounce;
-                case Ease.InCirc: return inCirc;
-                case Ease.InCubic: return inCubic;
-                case Ease.InElastic: return inElastic;
-                case Ease.InExpo: return inExpo;
-                case Ease.InQuad: return inQuad;
-                case Ease.InQuart: return inQuart;
-                case Ease.InQuint: return inQuint;
-                case Ease.InSine: return inSine;
-                case Ease.OutBack: return outBack;
-                case Ease.OutBounce: return outBounce;
-                case Ease.OutCirc: return outCirc;
-                case Ease.OutCubic: return outCubic;
-                case Ease.OutElastic: return outElastic;
-                case Ease.OutExpo: return outExpo;
-                case Ease.OutQuad: return outQuad;
-                case Ease.OutQuart: return outQuart;
-                case Ease.OutQuint: return outQuint;
-                case Ease.OutSine: return outSine;
-                case Ease.InOutBack: return inOutBack;
-                case Ease.InOutBounce: return inOutBounce;
-                case Ease.InOutCirc: return inOutCirc;
-                case Ease.InOutCubic: return inOutCubic;
-                case Ease.InOutElastic: return inOutElastic;
-                case Ease.InOutExpo: return inOutExpo;
-                case Ease.InOutQuad: return inOutQuad;
-                case Ease.InOutQuart: return inOutQuart;
-                case Ease.InOutQuint: return inOutQuint;
-                case Ease.InOutSine: return inOutSine;
-                default: return linear;
-            }
+		public static float InBack ( float t ) => t*t*t - t * math.sin(t*math.PI);
 
-            float linear(float t) => t;
+		public static float OutBack ( float t ) => 1f - InBack(1f-t);
 
-            float inBack(float t) => t * t * t - t * Mathf.Sin(t * Mathf.PI);
+		public static float InOutBack ( float t ) =>
+			t < 0.5f
+			? 0.5f * InBack(2f*t)
+			: 0.5f * OutBack(2f*t - 1f) + 0.5f;
 
-            float outBack(float t) => 1f - inBack(1f - t);
+		public static float InBounce ( float t ) => 1f - OutBounce(1f-t);
 
-            float inOutBack(float t) =>
-                t < 0.5f
-                    ? 0.5f * inBack(2f * t)
-                    : 0.5f * outBack(2f * t - 1f) + 0.5f;
+		public static float OutBounce ( float t ) =>
+			t < 4f / 11.0f ?
+				(121f * t * t) / 16.0f :
+			t < 8f / 11.0f ?
+				(363f / 40f * t * t) - (99f / 10f * t) + 17f / 5.0f :
+			t < 9f / 10f ?
+				(4356f / 361.0f * t * t) - (35442f / 1805.0f * t) + 16061f / 1805.0f :
+				(54f / 5.0f * t * t) - (513f / 25.0f * t) + 268f / 25.0f;
 
-            float inBounce(float t) => 1f - outBounce(1f - t);
+		public static float InOutBounce ( float t ) =>
+			t < 0.5f
+			? 0.5f * InBounce(2f*t)
+			: 0.5f * OutBounce(2f*t - 1f) + 0.5f;
 
-            float outBounce(float t) =>
-                t < 4f / 11.0f ?
-                    (121f * t * t) / 16.0f :
-                t < 8f / 11.0f ?
-                    (363f / 40.0f * t * t) - (99f / 10.0f * t) + 17f / 5.0f :
-                t < 9f / 10.0f ?
-                    (4356f / 361.0f * t * t) - (35442f / 1805.0f * t) + 16061f / 1805.0f :
-                    (54f / 5.0f * t * t) - (513f / 25.0f * t) + 268f / 25.0f;
+		public static float InCirc ( float t ) => 1f - math.sqrt(1f - (t*t));
 
-            float inOutBounce(float t) =>
-                t < 0.5f
-                    ? 0.5f * inBounce(2f * t)
-                    : 0.5f * outBounce(2f * t - 1f) + 0.5f;
+		public static float OutCirc ( float t ) => math.sqrt((2f - t) * t);
 
-            float inCirc(float t) => 1f - Mathf.Sqrt(1f - (t * t));
+		public static float InOutCirc ( float t ) =>
+			t < 0.5f
+			? 0.5f * (1 - math.sqrt(1f - 4f * (t * t)))
+			: 0.5f * (math.sqrt(-((2f * t) - 3f) * ((2f * t) - 1f)) + 1f);
 
-            float outCirc(float t) => Mathf.Sqrt((2f - t) * t);
+		public static float InCubic ( float t ) => t*t*t;
 
-            float inOutCirc(float t) =>
-                t < 0.5f
-                    ? 0.5f * (1 - Mathf.Sqrt(1f - 4f * (t * t)))
-                    : 0.5f * (Mathf.Sqrt(-((2f * t) - 3f) * ((2f * t) - 1f)) + 1f);
+		public static float OutCubic ( float t ) => InCubic(t - 1f) + 1f;
 
-            float inCubic(float t) => t * t * t;
+		public static float InOutCubic ( float t ) =>
+			t < 0.5f
+			? 4f * t*t*t
+			: 0.5f * InCubic(2f * t - 2f) + 1f;
 
-            float outCubic(float t) => inCubic(t - 1f) + 1f;
+		public static float InElastic ( float t ) => math.sin(13f * (math.PI * 0.5f) * t) * math.pow(2f, 10f * (t - 1f));
 
-            float inOutCubic(float t) =>
-                t < 0.5f
-                    ? 4f * t * t * t
-                    : 0.5f * inCubic(2f * t - 2f) + 1f;
+		public static float OutElastic ( float t ) => math.sin(-13f * (math.PI * 0.5f) * (t + 1)) * math.pow(2f, -10f * t) + 1f;
 
-            float inElastic(float t) => Mathf.Sin(13f * (Mathf.PI * 0.5f) * t) * Mathf.Pow(2f, 10f * (t - 1f));
+		public static float InOutElastic ( float t ) =>
+			t < 0.5f
+			? 0.5f * math.sin(13f * (math.PI * 0.5f) * (2f * t)) * math.pow(2f, 10f * ((2f * t) - 1f))
+			: 0.5f * (math.sin(-13f * (math.PI * 0.5f) * ((2f * t - 1f) + 1f)) * math.pow(2f, -10f * (2f * t - 1f)) + 2f);
 
-            float outElastic(float t) => Mathf.Sin(-13f * (Mathf.PI * 0.5f) * (t + 1)) * Mathf.Pow(2f, -10f * t) + 1f;
+		public static float InExpo ( float t ) => Mathf.Approximately(0f,t) ? t : math.pow(2f, 10f * (t - 1f));
 
-            float inOutElastic(float t) =>
-                t < 0.5f
-                    ? 0.5f * Mathf.Sin(13f * (Mathf.PI * 0.5f) * (2f * t)) * Mathf.Pow(2f, 10f * ((2f * t) - 1f))
-                    : 0.5f * (Mathf.Sin(-13f * (Mathf.PI * 0.5f) * ((2f * t - 1f) + 1f)) * Mathf.Pow(2f, -10f * (2f * t - 1f)) + 2f);
+		public static float OutExpo ( float t ) => Mathf.Approximately(1.0f, t) ? t : 1f - math.pow(2f, -10f * t);
 
-            float inExpo(float t) => Mathf.Approximately(0.0f, t) ? t : Mathf.Pow(2f, 10f * (t - 1f));
+		public static float InOutExpo ( float v ) =>
+			Mathf.Approximately(0f, v) || Mathf.Approximately(1.0f, v)
+				? v
+				: v < 0.5f
+					?  0.5f * math.pow(2f, (20f * v) - 10f)
+					: -0.5f * math.pow(2f, (-20f * v) + 10f) + 1f;
 
-            float outExpo(float t) => Mathf.Approximately(1.0f, t) ? t : 1f - Mathf.Pow(2f, -10f * t);
+		public static float InQuad ( float t ) => t*t;
 
-            float inOutExpo(float v) =>
-                Mathf.Approximately(0.0f, v) || Mathf.Approximately(1.0f, v)
-                    ? v
-                    : v < 0.5f
-                        ?  0.5f * Mathf.Pow(2f, (20f * v) - 10f)
-                        : -0.5f * Mathf.Pow(2f, (-20f * v) + 10f) + 1f;
+		public static float OutQuad ( float t ) => -t * (t - 2f);
 
-            float inQuad(float t) => t * t;
+		public static float InOutQuad ( float t ) =>
+			t < 0.5f
+			?  2f * t*t
+			: -2f * t*t + 4f * t - 1f;
 
-            float outQuad(float t) => -t * (t - 2f);
+		public static float InQuart ( float t ) => t*t*t*t;
 
-            float inOutQuad(float t) =>
-                t < 0.5f
-                    ?  2f * t * t
-                    : -2f * t * t + 4f * t - 1f;
+		public static float OutQuart ( float t )
+		{
+			float u = t - 1f;
+			return u*u*u * (1f - t) + 1f;
+		}
 
-            float inQuart(float t) => t * t * t * t;
+		public static float InOutQuart ( float t ) =>
+			t < 0.5f
+			? 8f * InQuart(t)
+			: -8f * InQuart(t - 1f) + 1f;
 
-            float outQuart(float t)
-            {
-                var u = t - 1f;
-                return u * u * u * (1f - t) + 1f;
-            }
+		public static float InQuint ( float t ) => t*t*t*t*t;
 
-            float inOutQuart(float t) =>
-                t < 0.5f
-                    ? 8f * inQuart(t)
-                    : -8f * inQuart(t - 1f) + 1f;
+		public static float OutQuint ( float t ) => InQuint(t - 1f) + 1f;
 
-            float inQuint(float t) => t * t * t * t * t;
+		public static float InOutQuint ( float t ) =>
+			t < 0.5f
+			? 16f * InQuint(t)
+			: 0.5f * InQuint(2f * t - 2f) + 1f;
 
-            float outQuint(float t) => inQuint(t - 1f) + 1f;
+		public static float InSine ( float t ) => math.sin((t - 1f) * (math.PI * 0.5f)) + 1f;
 
-            float inOutQuint(float t) =>
-                t < 0.5f
-                    ? 16f * inQuint(t)
-                    : 0.5f * inQuint(2f * t - 2f) + 1f;
+		public static float OutSine ( float t ) => math.sin(t * (math.PI * 0.5f));
 
-            float inSine(float t) => Mathf.Sin((t - 1f) * (Mathf.PI * 0.5f)) + 1f;
+		public static float InOutSine ( float t ) => 0.5f * (1f - math.cos(t * math.PI));
 
-            float outSine(float t) => Mathf.Sin(t * (Mathf.PI * 0.5f));
-
-            float inOutSine(float t) => 0.5f * (1f - Mathf.Cos(t * Mathf.PI));
-        }
     }
 }
